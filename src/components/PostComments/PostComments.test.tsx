dff3f321ec445cfebb7cb15aa8d813abc81f03ba
ViniSilv27 <vinicius.s.silv18@gmail.com>
2025-12-18 13:06:54 -0300

    exercicio finalizado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve permitir  a inserção de dois comentários", () => {
+    render(<PostComment />);
+    const commentInput = screen.getByTestId("comment-input");
+    const commentButton = screen.getByTestId("comment-button");
+    //inserir o primeiro comentário
+    fireEvent.change(commentInput, {
+      target: { value: "Primeiro comentário" },
     });
+    fireEvent.click(commentButton);
+    //inserir o segundo comentário
+    fireEvent.change(commentInput, { target: { value: "segundo comentario"}});
+    fireEvent.click(commentButton);
+    //verificar se os dois comentários foram inseridos
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
+    expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
+    expect(screen.getByText("segundo comentario")).toBeInTheDocument();
+
+
+  });
 });
